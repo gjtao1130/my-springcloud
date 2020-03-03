af57a881e3d88349940336a49ccc95e5fbdf33ea
gjtao1130 <gjtao1130@163.com>
2020-03-03 12:06:23 +0800

    20200302注册中心

--- provider-dept-8001/src/main/java/com/sky/ProviderDept8001Application.java
+++ provider-dept-8001/src/main/java/com/sky/ProviderDept8001Application.java
@@ -2,8 +2,10 @@ package com.sky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class ProviderDept8001Application {
 
     public static void main(String[] args) {
