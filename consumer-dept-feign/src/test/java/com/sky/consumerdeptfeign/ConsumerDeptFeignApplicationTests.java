ce23452201005a6e3095e8f567e2355091bfdaf4
gjtao1130 <gjtao1130@163.com>
2020-03-04 10:35:05 +0800

    20200303

---
+++ consumer-dept-feign/src/test/java/com/sky/consumerdeptfeign/ConsumerDeptFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.consumerdeptfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerDeptFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
